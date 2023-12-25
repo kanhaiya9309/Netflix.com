4691e9ec25d6742b9dd3f68ffca6d33c1afa7c42
Kanhaiya Gawali <124244655+kanhaiya9309@users.noreply.github.com>
2023-12-25 17:45:59 +0530

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,4 +1,4 @@
-let api_key = "79e114cce9060f23c98a1693dbd765f5";
+let api_key = "your-movie-database-api-key";
 
 let img_Url = "https://image.tmdb.org/t/p/w500";
 let original_img_url = "https://image.tmdb.org/t/p/original";
